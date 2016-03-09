1f9ed19f1b50f3be76b978bb3acff0c8d9125597
Aritra Sengupta <aritra55@gmail.com>
2016-03-09 09:42:49 -0600

    Using whitespace instead of tab

--- src/array-data-provider-factory.js
+++ src/array-data-provider-factory.js
@@ -1,31 +1,31 @@
 module.exports = function() {
-	var arrayDataProvider = {};
+    var arrayDataProvider = {};
 
-	arrayDataProvider.init = function(arr) {
-		arrayDataProvider.dataArray = arr;
-	};
+    arrayDataProvider.init = function(arr) {
+        arrayDataProvider.dataArray = arr;
+    };
 
-	arrayDataProvider.getDataProvider = function() {
-    	return function (q, cb) {
-    		var matches, substringRegex;
+    arrayDataProvider.getDataProvider = function() {
+        return function (q, cb) {
+            var matches, substringRegex;
 
-    		// an array that will be populated with substring matches
-    		matches = [];
+            // an array that will be populated with substring matches
+            matches = [];
 
-    		// regex used to determine if a string contains the substring `q`
-    		substrRegex = new RegExp(q, 'i');
+            // regex used to determine if a string contains the substring `q`
+            substrRegex = new RegExp(q, 'i');
 
-    		// iterate through the pool of strings and for any string that
-    		// contains the substring `q`, add it to the `matches` array
-    		$.each(arrayDataProvider.dataArray, function(i, str) {
-      			if (substrRegex.test(str)) {
-        			matches.push(str);
-      			}
-    		});
+            // iterate through the pool of strings and for any string that
+            // contains the substring `q`, add it to the `matches` array
+            $.each(arrayDataProvider.dataArray, function(i, str) {
+                if (substrRegex.test(str)) {
+                    matches.push(str);
+                }
+            });
 
-    		cb(matches);
-  		};
-  	};
+            cb(matches);
+        };
+    };
 
-  	return arrayDataProvider;	
+    return arrayDataProvider;   
 };
